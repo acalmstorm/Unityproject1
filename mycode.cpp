2f3fdaecdc6479894c271cc9c01c0a82a98c94a3
Jenil Nagrecha <75077531+Jenilnagrecha@users.noreply.github.com>
2022-10-25 22:43:29 +0530

    Create mycode.cpp

---
+++ mycode.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#define ll long long
+#define fr(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(false); cin.tie(NULL);
+int n;
+cin>>n;
+cout<<n%25<<endl;
+
+    return 0;
+}
